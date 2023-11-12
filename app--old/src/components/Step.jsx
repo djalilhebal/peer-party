707aad0fb4a107befe9766247e61bdf9865775fa
djalilhebal <abdeldjalil.hebal@gmail.com>
2023-11-12 01:23:46 +0100

    Remove app--old

--- app--old/src/components/Step.jsx
+++
@@ -1,9 +0,0 @@
-export default function Step({ completed = false, children }) {
-
-    return (
-        <div className={`step ${completed ? 'step--completed' : ''}`}>
-            {children}
-        </div>
-    );
-
-}
